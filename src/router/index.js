ce5c53a503c1269bcdae1bac3f0f3a2cec29e63c
Anatoliy Vasilyev <AnaAndVasilev@beeline.ru>
2021-10-25 23:59:26 +0300

    gh-pages

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
+    component: () => import(/* webpackChunkName: "home-tracking" */ '../views/Home.vue')
   },
   {
     path: '/:id',
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: '/cstore-fast/'
+}
